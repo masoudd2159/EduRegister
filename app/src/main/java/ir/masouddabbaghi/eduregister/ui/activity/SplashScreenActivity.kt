425499cc965e7b7857862e26aea02ef3f19a9744
Masoud Dabbaghi <MasoudD2159@gmail.com>
2024-09-15 18:24:43 +0330

    set up splash screen

--- app/src/main/java/ir/masouddabbaghi/eduregister/ui/activity/SplashScreenActivity.kt
+++ app/src/main/java/ir/masouddabbaghi/eduregister/ui/activity/SplashScreenActivity.kt
@@ -1,21 +1,49 @@
 package ir.masouddabbaghi.eduregister.ui.activity
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import dagger.hilt.android.AndroidEntryPoint
 import ir.masouddabbaghi.eduregister.R
+import ir.masouddabbaghi.eduregister.data.storage.SharedPreferencesHelper
+import ir.masouddabbaghi.eduregister.data.storage.SharedPreferencesKeys.KEY_ACCESS_TOKEN
+import ir.masouddabbaghi.eduregister.databinding.ActivitySplashScreenBinding
+import ir.masouddabbaghi.eduregister.ui.base.BaseActivity
+import javax.inject.Inject
 
-class SplashScreenActivity : AppCompatActivity() {
+@AndroidEntryPoint
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : BaseActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    private val splashDelay: Long = 2_000L // 2 seconds
+
+    @Inject
+    lateinit var sharedPreferencesHelper: SharedPreferencesHelper
+
+    override fun getLayoutResourceBinding(): View {
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding.version.text = getString(R.string.app_version, packageManager.getPackageInfo(packageName, 0).versionName)
+        if (sharedPreferencesHelper.getString(KEY_ACCESS_TOKEN, "").isEmpty()) {
+            sharedPreferencesHelper.clearSharedPreferences()
+            Handler(Looper.getMainLooper()).postDelayed({
+                val intent = Intent(this@SplashScreenActivity, AuthenticationActivity::class.java)
+                startActivity(intent)
+                finish() // Finish the SplashScreenActivity and Start AuthenticationActivity
+            }, splashDelay)
+        } else {
+            Handler(Looper.getMainLooper()).postDelayed({
+                val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish() // Finish the SplashScreenActivity and Start MainActivity
+            }, splashDelay)
         }
     }
 }
